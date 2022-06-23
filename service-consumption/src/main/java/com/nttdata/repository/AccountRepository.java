5e9a502a46bc5779930f1e9d89ae76340f23aa97
Jonathanmb27 <73472449+Jonathanmb27@users.noreply.github.com>
2022-06-22 23:59:31 -0500

    update

--- service-consumption/src/main/java/com/nttdata/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nttdata.repository;
-
-import com.nttdata.dao.Account;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface AccountRepository extends MongoRepository<Account,String> {
-}
