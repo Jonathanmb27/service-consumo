e4548a62846ad55cbbdf377cff965b60945e3232
Jonathanmb27 <73472449+Jonathanmb27@users.noreply.github.com>
2022-06-27 09:35:26 -0500

    update files with redis

--- service-consumo/src/main/java/com/nttdata/repository/AbstractRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nttdata.repository;
-
-import com.nttdata.model.dao.AbstractDocument;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AbstractRepository<T extends AbstractDocument> extends MongoRepository<T,String> {
-}
--- service-consumo/src/main/java/com/nttdata/repository/DetailRepository.java
+++
@@ -1,6 +0,0 @@
-package com.nttdata.repository;
-
-import com.nttdata.model.dao.Detail;
-
-public interface DetailRepository extends AbstractRepository<Detail>{
-}
