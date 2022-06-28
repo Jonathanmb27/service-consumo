0259d984dcc664f50558be74eec6c22eb1083f93
Jonathanmb27 <73472449+Jonathanmb27@users.noreply.github.com>
2022-06-27 23:45:27 -0500

    update data

--- service-consumo/src/main/java/com/nttdata/repository/YanquiAccountRepository.java
+++
@@ -1,6 +0,0 @@
-package com.nttdata.repository;
-
-import com.nttdata.model.dao.YanquiAccount;
-
-public interface YanquiAccountRepository extends AbstractRepository<YanquiAccount>{
-}
--- service-consumo/src/main/java/com/nttdata/service/DetailService.java
+++
@@ -1,6 +0,0 @@
-package com.nttdata.service;
-
-import com.nttdata.model.dao.Detail;
-
-public interface DetailService extends AbstractService<Detail>{
-}
